6b23fd81591240b283f17596855f2780e0fae149
duc nguyen <ngtrungduc29@gmail.com>
2023-02-24 11:31:09 +0700

    init project

---
+++ server.js
@@ -0,0 +1,11 @@
+const app = require("./src/app");
+
+const PORT = 8080
+
+const server = app.listen( PORT, () => {
+  console.log(`Server run on ${PORT}`);
+})
+
+process.on('SIGINT', () => {
+  server.close(() => console.log(`Exit Server Express`))
+})
