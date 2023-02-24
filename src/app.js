bc625f2bf6fd3e3a76efb4785a19361c6e28d955
duc nguyen <ngtrungduc29@gmail.com>
2023-02-24 15:16:18 +0700

    connect mongodb

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const compression = require('compression')
 const express = require('express')
 const { default: helmet } = require('helmet')
 const morgan = require('morgan')
+const { checkOverload } = require('./helpers/check.connect')
 const app = express()
 
 
@@ -11,6 +12,8 @@ app.use(helmet())
 app.use(compression())
 
 // init db
+require('./dbs/init.mongodb')
+checkOverload()
 
 // init routes
 app.get('/',  (req, res, next) => {
